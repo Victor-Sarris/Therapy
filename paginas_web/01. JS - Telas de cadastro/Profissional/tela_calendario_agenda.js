3bc471b1ee696e4e187de0e78896146bf3bdf9fb
Victor Sarrís Silva Santos <devvictorsarris@gmail.com>
2025-06-18 10:39:40 -0300

    criacoes de popup e adequacoes de telas do cliente e profissional. As telas de cadastro foram ajustadas para suas respectivas partes. Outr coisa que tbm foi ajustada, foi o logout. Elas estao funcionando nas principais telas de ambas as visoes

--- paginas_web/01. JS - Telas de cadastro/Profissional/tela_calendario_agenda.js
+++ paginas_web/01. JS - Telas de cadastro/Profissional/tela_calendario_agenda.js
@@ -15,3 +15,23 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+
+// modal de confirmação de agenda
+
+
+document.getElementById("salvarModalBnt").onclick = function () {
+  // Fecha o modal de notas
+  document.getElementById("modalNotas").style.display = "none";
+
+  // Abre o modal de confirmação
+  const modalConfirmacao = document.getElementById("modalConfirmacao");
+  modalConfirmacao.style.display = "block";
+
+  // Fecha o modal de confirmação após 3 segundos
+  setTimeout(() => {
+    modalConfirmacao.style.display = "none";
+  }, 3000);
+};
